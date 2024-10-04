93c29bdb9f2eff369b781a4fb80849c0a3aef546
ruby723 <molly723@naver.com>
2024-10-04 17:00:01 +0900

    계산기 func 추가

--- PracCalculator/Praccalculator/ViewController.swift
+++ PracCalculator/Praccalculator/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var operatorButton: UIButton!
     
+    func showAlert(message:String) {
+        let alert = UIAlertController(title:"알림", message:"\(message)", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title:"확인", style:.default) // 얼럿에 확인 버튼 추가
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+    
     @IBAction func selectOperator(_ sender: Any) {
         let actionSheet = UIAlertController(title:nil, message:nil, preferredStyle: .actionSheet)
         
@@ -47,26 +56,74 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func calculate(_ sender: Any) {
-        let a = Int(firstOperandField.text!)!
-        let b = Int(secondOperandField.text!)!
         
-        let op = operatorButton.title(for:.normal)!
+//        let a = Int(firstOperandField.text!)!
+//        let b = Int(secondOperandField.text!)!
+//        let op = operatorButton.title(for:.normal)!
         
-        if(op == "+") {
-            let result = a+b
-            resultLabel.text = "\(result)"
-        } else if op == "-" {
-            let result = a-b
-            resultLabel.text = "\(result)"
-        } else if op == "*" {
-            let result = a*b
-            resultLabel.text = "\(result)"
-        } else if op == "/" {
-            let result = a/b
-            resultLabel.text = "\(result)"
-        } else {
-            print("연산자 선택")
+        guard let text = firstOperandField.text, let a = Int(text) else {
+            
+            showAlert(message:"값을 입력해주세요")
+            return
         }
+        
+        guard let text = secondOperandField.text, let b = Int(text) else {
+            
+            showAlert(message:"값을 입력해주세요")
+            return
+        }
+        
+        guard let op = operatorButton.title(for:.normal), op != "?" else {
+            
+            showAlert(message:"연산자를 선택해주세요")
+            return
+        }
+        
+        
+//        if let text = firstOperandField.text, let a = Int(text) {
+//            
+//            if let text = secondOperandField.text, let b = Int(text) {
+//                
+//                if let op = operatorButton.title(for:.normal) {
+//                    
+//                    if(op == "+") {
+//                        let result = a+b
+//                        resultLabel.text = "\(result)"
+//                    } else if op == "-" {
+//                        let result = a-b
+//                        resultLabel.text = "\(result)"
+//                    } else if op == "*" {
+//                        let result = a*b
+//                        resultLabel.text = "\(result)"
+//                    } else if op == "/" {
+//                        let result = a/b
+//                        resultLabel.text = "\(result)"
+//                    } else {
+//                        print("연산자 선택")
+//                    }
+//                }
+//            }
+//        }
+        
+        var result : Int? = nil
+        switch op {
+            case "+" :
+                result = a + b
+            case "-" :
+                result = a - b
+            case "*" :
+                result = a * b
+            case "/" :
+                result = a / b
+        default :
+            print("연산자 선택")
+            break
+        }
+        
+        guard let result = result else {return}
+        
+        resultLabel.text = "\(result)"
+        
         
     }
     
